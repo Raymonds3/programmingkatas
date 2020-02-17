6a492cf4a67f4ee6dfca81f92ced42bc5a35619b
Raymonds3 <raymonds338@gmail.com>
2020-02-17 16:01:07 +0200

    commit2

--- src/main/java/Programkatas.java
+++ src/main/java/Programkatas.java
@@ -1,5 +1,7 @@
 import com.sun.org.apache.xpath.internal.objects.XNull;
 
+import java.util.ArrayList;
+
 public class Programkatas {
 
         public static void hello(String name){
@@ -38,8 +40,8 @@ public class Programkatas {
                 for(int j=hushtr-i;j>1;j--){
                     System.out.print(" ");
                 }
-                for(int j=0;j<=i;j++){
-                    System.out.print("# ");
+                for(int j=0;j<=i*2;j++){
+                    System.out.print("#");
 
                 }
 
@@ -47,28 +49,46 @@ public class Programkatas {
             }
         }
 
-        public static void longest(String[] lon) {
-
-            int maxLength = 0;
-            String longestString = "";
-            for (String l : lon) {
-                if (l.length() > maxLength) {
-                    maxLength = l.length();
-                    longestString = l;
+        public static void longest(String... lon) {
+            ArrayList<String> list = new ArrayList<String>();
+            int longest_length = 0;
+            for (String i : lon) {
+                int length = i.length();
+                if (length > longest_length) {
+                    longest_length = length;
+                    list.clear();
+                }
+                if (length == longest_length) {
+                    list.add(i);
                 }
             }
-            System.out.println(longestString);
+            for (String each:list) {
+
+                System.out.println(each);
+            }
         }
+
+        public static void frame(String... fra){
+
+            System.out.println("*********");
+            for(String i:fra){
+                System.out.println("* "+i);
+            }
+            System.out.println("*********");
+        }
+
 
 
         public static void main(String[] args){
 //        hello("Tshepo");
 //        evenOrOdd(3);
-//        square(2);
+//        square(4);
 //        triangle(2);
 //        isosceles(2);
-//        longest(new String[]{"the", "quick", "brown", "chickens", "fox", "ate", "my" });
-        longest(new String[]{"once", "upon", "a", "time"});
+//        longest("the", "quick", "brown", "chickens", "fox", "ate", "my");
+//        longest("once", "upon", "a", "time", "hi");
+        frame("Write","good","code","every","day");
+
         }
 }
 
