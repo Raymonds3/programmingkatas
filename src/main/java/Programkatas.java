73fd93c1c6b4fbc776f7dd958d42707205cd9fe6
Raymonds3 <raymonds338@gmail.com>
2020-02-18 11:01:50 +0200

    update commit

--- src/main/java/Programkatas.java
+++ src/main/java/Programkatas.java
@@ -1,6 +1,6 @@
-import com.sun.org.apache.xpath.internal.objects.XNull;
-
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Programkatas {
 
@@ -67,12 +67,43 @@ public class Programkatas {
                 System.out.println(each);
             }
         }
+        public static void combine(int a[], int b[]) {
+            int aLen = a.length;
+            int bLen = b.length;
+            int[] result = new int[aLen + bLen];
+            int i=0, j=0,k=0;
+            while(i<aLen&&j<bLen) {
+                result[k++]=a[i++];
+                result[k++]=b[j++];
+            }
+            while (i<aLen) {
+                result[k++]=a[i++];
+            }
+            while (j<bLen) {
+                result[k++]=b[j++];
+            }
+            System.out.println(Arrays.toString(result));
+        }
 
         public static void frame(String... fra){
 
             System.out.println("*********");
             for(String i:fra){
-                System.out.println("* "+i);
+                    if (i.length() == 5) {
+                        System.out.print("* " + i);
+                        System.out.print(" ");
+                        System.out.print("*\n");
+                    } else if (i.length() == 4) {
+                        System.out.print("* " + i);
+                        System.out.print("  ");
+                        System.out.print("*\n");
+                    } else if (i.length() == 3) {
+                        System.out.print("* " + i);
+                        System.out.print("   ");
+                        System.out.print("*\n");
+                    }else{
+                        continue;
+                    }
             }
             System.out.println("*********");
         }
@@ -80,15 +111,17 @@ public class Programkatas {
 
 
         public static void main(String[] args){
-//        hello("Tshepo");
-//        evenOrOdd(3);
-//        square(4);
-//        triangle(2);
-//        isosceles(2);
-//        longest("the", "quick", "brown", "chickens", "fox", "ate", "my");
-//        longest("once", "upon", "a", "time", "hi");
-        frame("Write","good","code","every","day");
-
+        hello("Tshepo");
+        evenOrOdd(3);
+        square(4);
+        triangle(4);
+        isosceles(4);
+        longest("the", "quick", "brown", "chickens", "fox", "ate", "my");
+        longest("once", "upon", "a", "time", "hi");
+            int a[] = {11,22,33};
+            int b[] = {1,2,3};
+        combine(a,b);
+        frame("Write", "good", "code", "every", "day");
         }
 }
 
